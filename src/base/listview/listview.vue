e55141ee9f8feb37fbe225b2607cfa547ead66c9
moshen_si <sizhiqian@sina.cn>
2017-07-09 23:01:35 +0800

    singer-detail

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -9,7 +9,8 @@
       <li v-for="group in data" class="list-group" ref="listGroup">
         <h2 class="list-group-title">{{group.title}}</h2>
         <ul>
-          <li v-for="item in group.items" class="list-group-item">
+          <li v-for="item in group.items" @click="selectItem(item)"
+          class="list-group-item">
             <img class="avatar" v-lazy="item.avatar" />
             <span class="name">{{item.name}}</span>
           </li>
@@ -77,6 +78,9 @@ export default {
     }
   },
   methods: {
+    selectItem(item) {
+      this.$emit('select', item)
+    },
     onShortcutTouchStart(e) {
       let anchorIndex = getData(e.target, 'index')
       let firstTouch = e.touches[0]
