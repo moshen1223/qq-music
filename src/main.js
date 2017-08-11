5e1170e0070ac421cdb110b36108e489720fa38a
MoshenSi <sizhiqian@sina.cn>
2017-08-11 12:15:33 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,9 +7,9 @@ import fastclick from 'fastclick'
 import VueLazyLoad from 'vue-lazyload'
 
 import 'common/stylus/index.styl'
-
+// 解决移动端点击延迟问题
 fastclick.attach(document.body)
-
+// 图片懒加载
 Vue.use(VueLazyLoad, {
   loading: require('common/image/default.png')
 })
